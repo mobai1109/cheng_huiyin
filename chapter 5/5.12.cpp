634d0c9a1dc51a45018fdf11a5202bbb3f5c6edf
LAPTOP-7LSRNHQD\chy <1916351769@qq.com>
2018-12-28 22:14:13 +0800

    第五章作业

---
+++ chapter 5/5.12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int chang;
+   int kuan;
+
+    for ( chang = 1; chang<= 10; chang++ )
+   {
+      for ( kuan = 1; kuan <= chang; kuan++ )
+      cout << "*";
+
+      cout<<endl;
+   }
+   }
