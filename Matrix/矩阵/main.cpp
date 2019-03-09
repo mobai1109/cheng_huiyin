2f408dcef3b330738fafdb09e400984c24639f7d
mobai1109 <1916351769@qq.com>
2019-03-09 20:37:24 +0800

    1701020406 程慧寅矩阵作业

---
+++ Matrix/矩阵/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include "Matrix.h"
+using namespace std;
+
+int main ()
+{
+    Martix a;
+    cout << "Enter martix number from 1-9:" <<endl;
+    cin >> a;
+
+    cout << "The martix was :";
+    cout<<a<<endl;
+}
