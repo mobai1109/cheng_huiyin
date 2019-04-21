629f9605a9718fdab7487b672464c38dcd624d2e
mobai1109 <1916351769@qq.com>
2019-04-21 19:33:50 +0800

    12.12

---
+++ chapter 12/12.12/HourlyEmployee.h
@@ -0,0 +1,25 @@
+#ifndef HOURLYEMPLOYEE_H_INCLUDED
+#define HOURLYEMPLOYEE_H_INCLUDED
+#include "Employee.h"
+
+class HourlyEmployee : public Employee
+{
+public:
+   static const int hoursPerWeek = 168;
+
+   HourlyEmployee( const string &, const string &,
+      const string &, int, int, int, double = 0.0, double = 0.0 );
+
+   void setWage( double );
+   double getWage() const;
+
+   void setHours( double );
+   double getHours() const;
+   virtual double earnings() const;
+   virtual void print() const;
+private:
+   double wage;
+   double hours;
+};
+
+#endif // HOURLYEMPLOYEE_H_INCLUDED
---
+++ chapter 12/12.12/SalariedEmployee.h
@@ -0,0 +1,18 @@
+#ifndef SALARIEDEMPLOYEE_H_INCLUDED
+#define SALARIEDEMPLOYEE_H_INCLUDED
+#include "Employee.h"
+
+class SalariedEmployee : public Employee
+{
+public:
+   SalariedEmployee( const string &, const string &,
+      const string &, int, int, int, double = 0.0 );
+
+   void setWeeklySalary( double );
+   double getWeeklySalary() const;
+   virtual double earnings() const;
+   virtual void print() const;
+private:
+   double weeklySalary;
+};
+#endif // SALARIEDEMPLOYEE_H_INCLUDED
